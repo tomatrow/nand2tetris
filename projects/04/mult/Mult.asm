31f05711241f01d29ab65785ff6b5120be15cf84
AJ Caldwell <tomatrow@gmail.com>
2015-02-18 15:05:49 -0800

    Optize Mult by swapping R0 and R1 if R0 is greater.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,13 +6,51 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
-
+// Initalize the output 
 @0
 D = A
 @R2
-M = D // Initalize the output 
+M = D 
 
+// if R0 == 0 then GOTO END
+@R0
+D = M
+@END
+D;JEQ
+
+// if R1 == 0 then GOTO END
+@R1
+D = M
+@END
+D;JEQ
+
+// if R1 >= R0 then don't swap 
+@R0
+D = D - M // D = R1 - R0
+@LOOP
+D;JGE // GOTO LOOP
+
+// R0 <-> R1
+
+// swap = R0 
+@R0
+D = M 
+@swap
+M = D 
+
+// R0 = R1 
+@R1 
+D = M
+@R0
+M = D
+
+// R1 = swap (R0)
+@swap
+D = M
+@R1
+M = D
+
+// multiplication 
 (LOOP)
     // R2 += R1
     @R1
@@ -28,3 +66,4 @@ M = D // Initalize the output
     D = M
     @LOOP 
     D;JGT
+(END)
