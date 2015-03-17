2c87f353c8befa68efc878d3a93d7be65a29af0f
AJ Caldwell <tomatrow@gmail.com>
2015-03-17 16:04:28 -0700

    Fixed spacing in Argument

--- projects/src/edu/miracosta/cs220/project07and08/Argument.java
+++ projects/src/edu/miracosta/cs220/project07and08/Argument.java
@@ -1,7 +1,7 @@
 package edu.miracosta.cs220.project07and08;
 
 /**
-Enumeration representing VM function arguments. 
+    Enumeration representing VM function arguments.
 */
 public enum Argument {
     SYMBOL, SEGMENT, FUNCTION_NAME, INTEGER, NULL;
