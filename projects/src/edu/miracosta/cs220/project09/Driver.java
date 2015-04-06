6a9f3646cc0dabd6de4dcf45c5dc376522b047b1
AJ Caldwell <tomatrow@gmail.com>
2015-04-06 15:50:24 -0700

    The intial project09 commit.

---
+++ projects/src/edu/miracosta/cs220/project09/Driver.java
@@ -0,0 +1,8 @@
+package edu.miracosta.cs220.project09;
+
+public class Driver {
+    public static void main(String[] args) {
+        Float f = new Float();
+        System.out.println("Hello");
+    }
+}
---
+++ projects/src/edu/miracosta/cs220/project09/Float.java
@@ -0,0 +1,7 @@
+package edu.miracosta.cs220.project09;
+
+public class Float {
+    public Float() {
+        
+    }
+}
